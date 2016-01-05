04d7a8eb42a04a07b0456e83de08048aabd5bfeb
Alvaro Massana <alvaromassana@gmail.com>
2016-01-05 12:52:31 +0100

    setup done

---
+++ app.rb
@@ -0,0 +1,9 @@
+
+class IdeaBoxApp < Sinatra::Base
+
+	register Sinatra::Reloader
+	get '/' do
+		"<h1>Hello, World!</h1><blockquote>I guess I always felt even if the world came to an end, McDonald's would still be open. <cite>Susan Beth Pfeffer</cite></blockquote>"
+
+	end
+end
